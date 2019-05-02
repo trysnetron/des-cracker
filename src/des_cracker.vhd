d0d02b7024e05da272d99ebb1b30124eecb6d8cd
Yohann Jacob Sandvik <sandvik@eurecom.fr>
2019-05-02 11:45:34 +0200

    add files

---
+++ src/des_cracker.vhd
@@ -0,0 +1,44 @@
+------------------------------------------------
+-----------------DES cracker--------------------
+------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Setting up entity of the des_cracker --  
+entity des_cracker is
+port(
+    aresetn        : in  std_ulogic                     -- **synchronous** active **low** reset from CPU part of Zynq core (the leading _a_ means AXI, not asynchronous)
+    s0_axi_araddr  : in  std_ulogic_vector(11 downto 0) -- read address from CPU (12 bits = 4kB)                                                                        
+    s0_axi_arvalid : in  std_ulogic                     -- read address valid from CPU                                                                                  
+    s0_axi_arready : out std_ulogic                     -- read address acknowledge to CPU                                                                              
+    s0_axi_awaddr  : in  std_ulogic_vector(11 downto 0) -- write address from CPU (12 bits = 4kB)                                                                       
+    s0_axi_awvalid : in  std_ulogic                     -- write address valid flag from CPU                                                                            
+    s0_axi_awready : out std_ulogic                     -- write address acknowledge to CPU                                                                             
+    s0_axi_wdata   : in  std_ulogic_vector(31 downto 0) -- write data from CPU                                                                                          
+    s0_axi_wstrb   : in  std_ulogic_vector(3 downto 0)  -- write byte enables from CPU                                                                                  
+    s0_axi_wvalid  : in  std_ulogic                     -- write data and byte enables valid from CPU                                                                   
+    s0_axi_wready  : out std_ulogic                     -- write data and byte enables acknowledge to CPU                                                               
+    s0_axi_rdata   : out std_ulogic_vector(31 downto 0) -- read data response to CPU                                                                                    
+    s0_axi_rresp   : out std_ulogic_vector(1 downto 0)  -- read status response (OKAY, EXOKAY, SLVERR or DECERR) to CPU                                                 
+    s0_axi_rvalid  : out std_ulogic                     -- read data and status response valid flag to CPU                                                              
+    s0_axi_rready  : in  std_ulogic                     -- read response acknowledge from CPU                                                                           
+    s0_axi_bresp   : out std_ulogic_vector(1 downto 0)  -- write status response (OKAY, EXOKAY, SLVERR or DECERR) to CPU                                                
+    s0_axi_bvalid  : out std_ulogic                     -- write status response valid to CPU                                                                           
+    s0_axi_bready  : in  std_ulogic                     -- write response acknowledge from CPU                                                                          
+    irq            : out std_ulogic                     -- interrupt request to CPU                                                                                     
+    led            : out std_ulogic_vector(3 downto 0)  -- wired to the four user LEDs                                                                                  
+);
+end entity des_cracker;
+
+-- Setting up architecture of the des_cracker
+architecture rtl of des_cracker is
+
+    signal p   : std_ulogic_vector(63 downto 0); -- The plaintext           base adress: 0x000
+    signal c   : std_ulogic_vector(63 downto 0); -- The ciphertext          base adress: 0x008
+    signal k0  : std_ulogic_vector(55 downto 0); -- The starting secret key base adress: 0x010
+    signal k   : std_ulogic_vector(55 downto 0); -- The current secret key  base adress: 0x018
+    signal k1  : std_ulogic_vector(55 downto 0); -- The found secret key    base adress: 0x020
+
+begin 
+end architecture rtl;
--- src/des_pkg.vhd
+++ src/des_pkg.vhd
@@ -1,55 +1,122 @@
 package des_pkg is
-  subtype w28 is std_ulogic_vector(1 to 28);
-  subtype w32 is std_ulogic_vector(1 to 32);
-  subtype w48 is std_ulogic_vector(1 to 48);
-  subtype w56 is std_ulogic_vector(1 to 56);
-  subtype w64 is std_ulogic_vector(1 to 64);
+    subtype w28 is std_ulogic_vector(1 to 28);
+    subtype w32 is std_ulogic_vector(1 to 32);
+    subtype w48 is std_ulogic_vector(1 to 48);
+    subtype w56 is std_ulogic_vector(1 to 56);
+    subtype w64 is std_ulogic_vector(1 to 64);
+    subtype w728 is std_ulogic_vector(1 to 768);
 
-  type ip_t is array(1 to 64) of natural;
+    type ip_t is array(1 to 64) of natural range 1 to 64;
+    type pc1_t is array(1 to 56) of natural range 1 to 64;
+    type pc2_t is array(1 to 48) of natural range 1 to 56;
 
-  constant ip_table : ip_t := (...);
+    constant ip_table : ip_t := (
+        58, 50, 42, 34, 26, 18, 10,  2, 
+        60, 52, 44, 36, 28, 20, 12,  4,
+        62, 54, 46, 38, 30, 22, 14,  6,
+        64, 56, 48, 40, 32, 24, 16,  8,
+        57, 49, 41, 33, 25, 17,  9,  1,
+        59, 51, 43, 35, 27, 19, 11,  3,
+        61, 53, 45, 37, 29, 21, 13,  5,
+        63, 55, 47, 39, 31, 23, 15,  7
+    );
 
-  function left_shift(w:w28; amount:natural) return w28;
-  function right_shift(w:w28; amount:natural) return w28;
+    constant pc1_table : pc1_t := (
+        57, 49, 41, 33, 25, 17, 9,
+         1, 58, 50, 42, 34, 26, 18,
+        10,  2, 59, 51, 43, 35, 27,
+        19, 11,  3, 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+         7, 62, 54, 46, 38, 30, 22,
+        14,  6, 61, 53, 45, 37, 29,
+        21, 13,  5, 28, 20, 12,  4
+    );
+    constant pc2_table : pc2_t := (
+        14, 17, 11, 24,  1,  5,
+         3, 28, 15,  6, 21, 10,
+        23, 19, 12,  4, 26,  8,
+        16,  7, 27, 20, 13,  2,
+        41, 52, 31, 37, 47, 55,
+        30, 40, 51, 45, 33, 48,
+        44, 49, 39, 56, 34, 53,
+        46, 42, 50, 36, 29, 32
+    );
+    
+    function left_shift(w:w28; amount:natural) return w28;
+    function right_shift(w:w28; amount:natural) return w28;
+    function sub_key_gen(key:w64; return w728;
 
-  function p(w:w32) return w32;
-  function f(r:w32; rk: w48) return w32;
-  function des(p:w64; k:w64) return w64;
-  function ip(p:w64) return w64;
+    function p(w:w32) return w32;
+    function f(r:w32; rk: w48) return w32;
+    function des(p:w64; k:w64) return w64;
+    function ip(p:w64) return w64;
 
 end package des_pkg;
 
-package body des_pkg is
-  
-  function left_shift(w:w28; amount:natural) return w28 is
-    if amount = 2 then
-      return w(3 to 28) & w(1 to 2);
-    elsif amount = 1 then
-      return w(2 to 28) & w(1);
-    else
-      assert false report "ERROR" severity failure;
-    end if;
-  end left_shift;
+package body des_pkg is 
+    
+    function sub_key_gen(key:w64; return w728 is
+        variable permuted_key:w56;
+        variable c:w48;
+        variable d:w48;
+        variable concatenated_pair:w56;
+        variable result:w728;
+
+    begin
+        -- permuting key according to table PC-1
+        for i in 1 to 56 loop
+            permuted_key(i) := key(pc1_table(i));
+        end loop;
+        -- Generating subkeys
+        c := permuted_key(1 to 28);
+        d := permuted_key(29 to 56);
+        for i in 1 to 16 loop
+            if i = 1 or i = 2 or i = 9 or i = 16 then
+                c := left_shift(c, 1);
+                d := left_shift(d, 1);
+            else 
+                c := left_shift(c, 2);
+                d := left_shift(d, 2);
+            end if;
+            concatenated_pair := c & d;
+            for j in 1 to 48 loop
+                result(i*48 - 1 + j) := concatenated_pair(pc2_table(j));
+            end loop;
+        end loop;
+        return result;
+    end function;
+
+    function left_shift(w:w28; amount:natural) return w28 is
+        if amount = 2 then
+            return w(3 to 28) & w(1 to 2);
+        elsif amount = 1 then
+            return w(2 to 28) & w(1);
+        else
+            assert false report "ERROR" severity failure;
+        end if;
+    end left_shift;
 
 
-  function right_shift(w:w28; amount:natural) return w28 is
-    if amount = 2 then
-      return w(27 to 28) & w(1 to 26);
-    elsif amount = 1 then
-      return w(28) & w(1 to 27);
-    else
-      assert false report "ERROR" severity failure;
-    end if;
-  end right_shift;
+    function right_shift(w:w28; amount:natural) return w28 is
+        if amount = 2 then
+            return w(27 to 28) & w(1 to 26);
+        elsif amount = 1 then
+            return w(28) & w(1 to 27);
+        else
+            assert false report "ERROR" severity failure;
+        end if;
+    end right_shift;
 
 
-  function ip(p:w64) return w64 is
-    variable result:w64;
-  begin
-    for i in 1 to 64 loop
-      result(i) := w(ip_table(i));
-    end loop;
-    return result;
-  end function;
+    function ip(p:w64) return w64 is
+        variable result:w64;
+    begin
+        for i in 1 to 64 loop
+            result(i) := w(ip_table(i));
+        end loop;
+        return result;
+    end function;
 
 end package body des_pkg;
+
+-- vim: set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab textwidth=0:
