1580690ff13bd98a622381ba9622979bbfa3ff09
Yohann Jacob Sandvik <sandvik@eurecom.fr>
2019-05-14 12:15:01 +0200

    changed sim_script to compile engine

--- sim_script.sh
+++ sim_script.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/packages/LabSoC/Xilinx/bin
 src=./src
 vlib work
 vmap work work
-vcom -2008 -work work $src/des_pkg.vhd $src/des_sim.vhd
-vsim -c -do "run -all; quit;" des_sim
+vcom -2008 -work work $src/des_pkg.vhd $src/des_engine.vhd $src/des_engine_sim.vhd
+#vsim -c -do "run -all; quit;" des_sim
+vsim des_engine_sim
 # . sim_script
