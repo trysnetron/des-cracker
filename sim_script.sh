815f1ef2da3e102e775427388d73e04d5bc0cae8
Yohann Jacob Sandvik <sandvik@eurecom.fr>
2019-05-14 13:41:39 +0200

    change name of sim-file for package functions

--- sim_script.sh
+++ sim_script.sh
@@ -4,6 +4,6 @@ src=./src
 vlib work
 vmap work work
 vcom -2008 -work work $src/des_pkg.vhd $src/des_engine.vhd $src/des_engine_sim.vhd
-#vsim -c -do "run -all; quit;" des_sim
-vsim des_engine_sim
+#vsim -c -do "run -all; quit;" des_pkg_sim # Simulutaion of package functions
+vsim des_engine_sim # Simulation of engine
 # . sim_script
