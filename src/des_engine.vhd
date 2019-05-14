2c90b4bac0973f34b1cf1a666d3bcfd0f7b9ba0e
Yohann Jacob Sandvik <sandvik@eurecom.fr>
2019-05-14 12:14:27 +0200

    add hardware entity for des_engine

---
+++ src/des_engine.vhd
@@ -0,0 +1,45 @@
+------------------------------------------------
+-----------------DES cracker--------------------
+---by Trym Sneltvedt and Yohann Jacob Sandvik---
+------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.des_pkg.all;
+
+-- Setting up entity of the engine --  
+entity des_engine is
+port(
+    clk         : in  std_ulogic; -- axi4 clock from one of the cortex CPUs
+    sresetn     : in  std_ulogic; -- **synchronous** active **low** reset from CPU part of Zynq core (the leading _a_ means AXI, not asynchronous)
+    plain_txt   : in  w64;
+    key         : in  w64;
+    cipher_txt  : out w64
+);
+end entity des_engine;
+
+-- Setting up architecture of the des_engine --
+architecture rtl of des_engine is
+    
+
+begin 
+    
+    process(clk)
+        variable acc: w64; -- Intermediate storage
+        variable sk: w768; -- subkeys
+    begin
+        if rising_edge(clk) then
+            if sresetn = '0' then
+                cipher_txt <= (others => '0');
+            else
+                sk := sub_key_gen(key);
+                acc := ip(plain_txt);
+                for i in 0 to 15 loop
+                    acc := des_step(sk(i*48 + 1 to (i + 1)*48 ), acc(1 to 32), acc(33 to 64));
+                end loop;
+                cipher_txt <= iip(acc);
+            end if;
+        end if;
+    end process;
+
+end architecture rtl;
---
+++ src/des_engine_sim.vhd
@@ -0,0 +1,46 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.des_pkg.all;
+
+entity des_engine_sim is
+port(
+    ciph : out w64 
+);
+end entity des_engine_sim;
+
+architecture sim of des_engine_sim is
+
+    constant period : time := (1000 ns);
+    constant plain_txt : w64 := "0000000100100011010001010110011110001001101010111100110111101111";
+    constant key : w64 := "0001001100110100010101110111100110011011101111001101111111110001";
+    
+    signal clk : std_ulogic;
+    signal sresetn : std_ulogic;
+    signal cipher_txt : w64;
+
+begin
+
+    process
+    begin
+        clk <= '0';
+        wait for period / 2.0;
+        clk <= '1';
+        wait for period / 2.0;
+    end process;
+
+    sresetn <= '1';
+
+    engine: entity work.des_engine(rtl)
+    port map(
+        clk         => clk,
+        sresetn     => sresetn,
+        plain_txt   => plain_txt,
+        key         => key, 
+        cipher_txt  => cipher_txt
+    );
+
+    ciph <= cipher_txt;
+
+
+end architecture sim; 
