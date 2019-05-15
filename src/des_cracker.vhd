354a324f3c38332b0c805626949ac5c97db11d32
Yohann Jacob Sandvik <sandvik@eurecom.fr>
2019-05-15 17:53:35 +0200

    fix syntax

--- src/des_cracker.vhd
+++ src/des_cracker.vhd
@@ -9,25 +9,38 @@ use ieee.numeric_std.all;
 -- Setting up entity of the des_cracker --  
 entity des_cracker is
 port(
-    aresetn        : in  std_ulogic;                     -- **synchronous** active **low** reset from CPU part of Zynq core (the leading _a_ means AXI, not asynchronous)
-    s0_axi_araddr  : in  std_ulogic_vector(11 downto 0); -- read address from CPU (12 bits = 4kB)                                                                        
-    s0_axi_arvalid : in  std_ulogic;                     -- read address valid from CPU                                                                                  
-    s0_axi_arready : out std_ulogic;                     -- read address acknowledge to CPU                                                                              
-    s0_axi_awaddr  : in  std_ulogic_vector(11 downto 0); -- write address from CPU (12 bits = 4kB)                                                                       
-    s0_axi_awvalid : in  std_ulogic;                     -- write address valid flag from CPU                                                                            
-    s0_axi_awready : out std_ulogic;                     -- write address acknowledge to CPU                                                                             
-    s0_axi_wdata   : in  std_ulogic_vector(31 downto 0); -- write data from CPU                                                                                          
-    s0_axi_wstrb   : in  std_ulogic_vector(3 downto 0);  -- write byte enables from CPU                                                                                  
-    s0_axi_wvalid  : in  std_ulogic;                     -- write data and byte enables valid from CPU                                                                   
-    s0_axi_wready  : out std_ulogic;                     -- write data and byte enables acknowledge to CPU                                                               
-    s0_axi_rdata   : out std_ulogic_vector(31 downto 0); -- read data response to CPU                                                                                    
-    s0_axi_rresp   : out std_ulogic_vector(1 downto 0);  -- read status response (OKAY, EXOKAY, SLVERR or DECERR) to CPU                                                 
-    s0_axi_rvalid  : out std_ulogic;                     -- read data and status response valid flag to CPU                                                              
-    s0_axi_rready  : in  std_ulogic;                     -- read response acknowledge from CPU                                                                           
-    s0_axi_bresp   : out std_ulogic_vector(1 downto 0);  -- write status response (OKAY, EXOKAY, SLVERR or DECERR) to CPU                                                
-    s0_axi_bvalid  : out std_ulogic;                     -- write status response valid to CPU                                                                           
-    s0_axi_bready  : in  std_ulogic;                     -- write response acknowledge from CPU                                                                          
-    irq            : out std_ulogic;                     -- interrupt request to CPU                                                                                     
+    aclk           : in  std_ulogic;                     -- master clock
+    aresetn        : in  std_ulogic;                     -- synchronous active low reset
+
+    -- Read address signals
+    s0_axi_araddr  : in  std_ulogic_vector(11 downto 0); -- read address
+    s0_axi_arvalid : in  std_ulogic;                     -- read address valid
+    s0_axi_arready : out std_ulogic;                     -- read address acknowledge
+
+    -- Write address signals
+    s0_axi_awaddr  : in  std_ulogic_vector(11 downto 0); -- write address
+    s0_axi_awvalid : in  std_ulogic;                     -- write address valid flag
+    s0_axi_awready : out std_ulogic;                     -- write address acknowledge
+
+    -- Read data signals
+    s0_axi_wdata   : in  std_ulogic_vector(31 downto 0); -- write data
+    s0_axi_wstrb   : in  std_ulogic_vector(3 downto 0);  -- write byte enables
+    s0_axi_wvalid  : in  std_ulogic;                     -- write data and byte enables valid
+    s0_axi_wready  : out std_ulogic;                     -- write data and byte enables acknowledge
+
+    -- Write data signals
+    s0_axi_rdata   : out std_ulogic_vector(31 downto 0); -- read data response to CPU
+    s0_axi_rresp   : out std_ulogic_vector(1 downto 0);  -- read status response (OKAY, EXOKAY, SLVERR or DECERR)
+    s0_axi_rvalid  : out std_ulogic;                     -- read data and status response valid flag
+    s0_axi_rready  : in  std_ulogic;                     -- read response acknowledge
+
+    -- Write response signals
+    s0_axi_bresp   : out std_ulogic_vector(1 downto 0);  -- write status response (OKAY, EXOKAY, SLVERR or DECERR)
+    s0_axi_bvalid  : out std_ulogic;                     -- write status response valid
+    s0_axi_bready  : in  std_ulogic;                     -- write response acknowledge
+
+
+    irq            : out std_ulogic;                     -- interrupt request
     led            : out std_ulogic_vector(3 downto 0);  -- wired to the four user LEDs                                                                                  
 );
 end entity des_cracker;
